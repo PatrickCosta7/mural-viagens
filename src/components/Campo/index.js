d0e92d7830c4f8de5c2814b96733a1c6c739bf10
Patrick Costa <pl.costa@hotmail.com>
2023-02-21 11:08:13 -0300

    Update

--- src/components/Campo/index.js
+++ src/components/Campo/index.js
@@ -1,18 +1,19 @@
 import './Campo.css'
 
-const Campo = ({label, valor, placeholder, obrigatorio, aoAlterado}) => {
+const Campo = ({label, valor, placeholder, obrigatorio, aoAlterado, type="text"}) => {
     
     const aoDigitado = (evento) => {
         aoAlterado(evento.target.value)
     }
 
     return(
-        <div className='campo'>
+        <div className={`campo campo-${type}`}>
             <label>
                 {label}
             </label>
             <input
                 required={obrigatorio}
+                type={type}
                 value={valor}
                 placeholder={placeholder}
                 onChange={aoDigitado}
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -6,19 +6,34 @@ import './Formulario.css';
 const Formulario = (props) => {
 
     const [nome, setNome] = useState('');
+    const [data, setData] = useState('');
+    const [viagem, setViagem] = useState('');
+    const [nomePais, setNomePais] = useState('');
+    const [corPrincipal, setCorPrincipal] = useState('');
+    const [corSecundaria, setCorSecundaria] = useState('');
 
-    const salvando = (evento) => {
+    const salvandoViagem = (evento) => {
         evento.preventDefault();
         props.novoLocal({
-            nome
+            nome,
+            data
         })
         setNome('');
+        setData('');
 
+    }
+
+    const salvandoPais = (evento) => {
+        evento.preventDefault();
+        props.novoLocal({
+            nomePais
+        })
+        setNomePais('');
     }
 
     return(
         <section className='formulario'>
-            <form onSubmit={salvando}>
+            <form onSubmit={salvandoViagem}>
                 <h2>Preencha os dados para postar sua viagem no mural</h2>
                 <Campo
                     obrigatorio
@@ -28,11 +43,62 @@ const Formulario = (props) => {
                     aoAlterado={valor => setNome(valor)}
                 />
 
+                <Campo
+                    obrigatorio
+                    label="Descrição"
+                    placeholder="Descreva sua viagem"
+                    valor={viagem}
+                    aoAlterado={valor => setViagem(valor)}
+                />
+
+                <Campo
+                    obrigatorio
+                    type='date'
+                    label="Data da viajem"
+                    valor={data}
+                    aoAlterado={valor => setData(valor)}
+                />
+
+                
+
                 <Botao>
                     Fixar viagem
                 </Botao>
 
             </form>
+
+            <form onSubmit={salvandoPais}>
+                <h2>Adicione um pais viajado</h2>
+                <Campo
+                    obrigatorio
+                    label="Nome do País"
+                    placeholder="Digite aqui o nome do País"
+                    valor={nomePais}
+                    aoAlterado={valor => setNomePais(valor)}
+                />
+
+                <Campo
+                    obrigatorio
+                    type='color'
+                    label="Cor principal"
+                    valor={corPrincipal}
+                    aoAlterado={valor => setCorPrincipal(valor)}
+                />
+                
+                <Campo
+                    obrigatorio
+                    type='color'
+                    label="Cor Secundária"
+                    valor={corSecundaria}
+                    aoAlterado={valor => setCorSecundaria(valor)}
+                />
+
+                <Botao>
+                    Adicionar País
+                </Botao>
+
+            </form>
+
         </section>
     )
 
